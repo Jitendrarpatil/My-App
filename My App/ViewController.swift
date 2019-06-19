4384dd0d75faaaa6c09caa21a664fd88be2c0242
Jitendra Patil <jitendrarpatil11@gmail.com>
2019-06-19 21:15:08 +0530

    Initial Commit

---
+++ My App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My App
+//
+//  Created by Jitendra Patil on 19/06/19.
+//  Copyright © 2019 Jitendra Patil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
